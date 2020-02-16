47fdcb018b0502a4be80a79ef927e4e5e0ae63bb
Albert Lockett <albert.lockett@sonraisecurity.com>
2020-02-16 18:49:18 -0400

    now TLS enabled

--- app/graphql-client/index.ts
+++ app/graphql-client/index.ts
@@ -3,7 +3,10 @@ import ApolloClient from 'apollo-boost';
 const { host } = window.location;
 const uri = host.startsWith('localhost')
     ? 'http://localhost/graphql'
-    : `http://graphql.${host}/graphql`;
+    : `https://graphql.${host}/graphql`;
+
+// eslint-disable-next-line no-console
+console.log('graphQL URI: ' + uri);
 
 const client = new ApolloClient({ uri });
 
